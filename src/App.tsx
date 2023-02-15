4f1e93d7f2dd5287f574de112034f4787abe5e5d
Kishan Suvarna <kishansuvarna09@gmail.com>
2023-02-16 01:03:13 +0530

    Edit Note module

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { useLocalStorage } from './useLocalStorage';
 import { useMemo } from 'react';
 import { v4 as uuidV4 } from 'uuid';
 import NoteList from './NoteList';
+import NoteLayout from './NoteLayout';
+import { Note } from './Note';
+import EditNote from './EditNote';
 
 export type Note = {
   id: string;
@@ -54,6 +57,22 @@ function App() {
     });
   }
 
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return {
+            ...note,
+            ...data,
+            tagIds: tags.map((tag) => tag.id),
+          };
+        } else {
+          return note;
+        }
+      });
+    });
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -61,7 +80,12 @@ function App() {
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<NoteList availableTags={tags} />} />
+        <Route
+          path="/"
+          element={
+            <NoteList availableTags={tags} notes={notesWithTags} />
+          }
+        />
         <Route
           path="/new"
           element={
@@ -72,9 +96,21 @@ function App() {
             />
           }
         />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
+        <Route
+          path="/:id"
+          element={<NoteLayout notes={notesWithTags} />}
+        >
+          <Route index element={<Note />} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={onUpdateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
